4555141ebdf01b39b66dfd7ab35283aa71ad3656
KROOKIMWOOBIN <woodol4540@naver.com>
2025-11-13 22:00:33 +0900

    9

---
+++ src/main/java/javacore/intermediate/ex9/MyDateMain.java
@@ -0,0 +1,40 @@
+package javacore.intermediate.ex9;
+
+import lombok.Getter;
+import lombok.ToString;
+
+public class MyDateMain {
+    public static void main(String[] args) {
+        MyDate date1 = new MyDate(2025, 11, 13);
+        MyDate date2 = date1;
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+        date2 = date2.withDay(14);
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+    }
+}
+
+class MyDate {
+    private final int year;
+    private final int month;
+    private final int day;
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+    public MyDate withYear(int newYear) {
+        return new MyDate(newYear, month, day);
+    }
+    public MyDate withMonth(int newMonth) {
+        return new MyDate(year, newMonth, day);
+    }
+    public MyDate withDay(int newDay) {
+        return new MyDate(year, month, newDay);
+    }
+    @Override
+    public String toString() {
+        return "year : " + year + ", month : " + month + ", day : " + day;
+    }
+}
