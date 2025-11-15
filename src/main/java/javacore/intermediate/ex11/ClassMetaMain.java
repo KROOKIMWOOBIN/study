5cb3de6fc43e3c4777fb1efd37e056b0baa48ed0
KROOKIMWOOBIN <woodol4540@naver.com>
2025-11-15 16:27:11 +0900

    11

--- src/main/java/javacore/intermediate/ex11/ClassMetaMain.java
+++ src/main/java/javacore/intermediate/ex11/ClassMetaMain.java
@@ -6,27 +6,27 @@ import java.lang.reflect.Method;
 public class ClassMetaMain {
     public static void main(String[] args) throws Exception {
         // Class 조회
-        Class class1 = String.class; // 1. 클래스에서 조회
-        Class class2 = new String().getClass(); // 2. 인스턴에서 조회
-        Class class3 = Class.forName("java.lang.String"); // 3. 문자열로 조회
+        Class clazz = String.class; // 1. 클래스에서 조회
+        // Class clazz = new String().getClass(); // 2. 인스턴에서 조회
+        // Class clazz = Class.forName("java.lang.String"); // 3. 문자열로 조회
 
         // 모든 필드 출력
-        Field[] fields = class1.getDeclaredFields();
+        Field[] fields = clazz.getDeclaredFields();
         for(Field field : fields) {
             System.out.println("field : " + field);
         }
 
         // 모든 메서드 출력
-        Method[] methods = class1.getDeclaredMethods();
+        Method[] methods = clazz.getDeclaredMethods();
         for(Method method : methods) {
             System.out.println("method : " + method);
         }
 
         // 상위 클래스 정보 출력
-        System.out.println("SuperClass : " + class1.getSuperclass().getName());
+        System.out.println("SuperClass : " + clazz.getSuperclass().getName());
 
         // 인터페이스 정보 출력
-        Class[] interfaces = class1.getInterfaces();
+        Class[] interfaces = clazz.getInterfaces();
         for(Class i : interfaces) {
             System.out.println("Interface : " + i);
         }
