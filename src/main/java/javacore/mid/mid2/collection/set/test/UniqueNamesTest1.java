f54bccdd169b1b8b81618a45caaa2926e4e25050
KROOKIMWOOBIN <woodol4540@naver.com>
2025-12-15 20:57:28 +0900

    20251215

---
+++ src/main/java/javacore/mid/mid2/collection/set/test/UniqueNamesTest1.java
@@ -0,0 +1,16 @@
+package javacore.mid.mid2.collection.set.test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UniqueNamesTest1 {
+    public static void main(String[] args) {
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+        Set<Integer> set = new HashSet<>();
+        for (Integer integer : inputArr) {
+            set.add(integer);
+        }
+        System.out.println("set = " + set);
+    }
+}
