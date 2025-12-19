d39601a94624e388d5c688e1d1137fc93e855a82
KROOKIMWOOBIN <woodol4540@naver.com>
2025-12-19 22:16:22 +0900

    20251219

---
+++ src/main/java/javacore/mid/mid2/collection/iterator/MyArrayMain.java
@@ -0,0 +1,8 @@
+package javacore.mid.mid2.collection.iterator;
+
+public class MyArrayMain {
+    public static void main(String[] args) {
+        MyArray myArray = new MyArray(new int[]{1, 2, 3, 4});
+        myArray.iterator();
+    }
+}
