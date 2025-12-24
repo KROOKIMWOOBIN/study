57d3a15f4266d68495470fd677cb3d81095d4df1
KROOKIMWOOBIN <woodol4540@naver.com>
2025-12-24 23:39:38 +0900

    20251224

--- src/main/java/javacore/thread/control/ThreadInfoMain.java
+++ src/main/java/javacore/thread/control/ThreadInfoMain.java
@@ -1,5 +1,7 @@
 package javacore.thread.control;
 
+import javacore.thread.start.HelloRunnable;
+
 import static javacore.thread.util.MyLogger.log;
 
 public class ThreadInfoMain {
@@ -11,5 +13,13 @@ public class ThreadInfoMain {
         log("mainThread.get: " + mainThread.getPriority());
         log("mainThread.getThreadGroup: " + mainThread.getThreadGroup());
         log("mainThread.getState: " + mainThread.getState());
+
+        Thread myThread = new Thread(new HelloRunnable(), "myThread");
+        log("myThread: " + myThread);
+        // log("myThread.threadId: " + myThread.threadId()); // 21 출력
+        log("myThread.getName: " + myThread.getName());
+        log("myThread.get: " + myThread.getPriority());
+        log("myThread.getThreadGroup: " + myThread.getThreadGroup());
+        log("myThread.getState: " + myThread.getState());
     }
 }
