1fc124a926f3549105f788b4fcbacdb261fcb2d1
KROOKIMWOOBIN <woodol4540@naver.com>
2025-12-26 20:27:07 +0900

    20251226

---
+++ src/main/java/javacore/thread/volatile1/VolatileFlagMain.java
@@ -0,0 +1,36 @@
+package javacore.thread.volatile1;
+
+import static javacore.thread.util.MyLogger.log;
+import static javacore.thread.util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag: " + task.runFlag);
+        t.start();
+        sleep(1_000);
+        log("runFlag => false");
+        task.runFlag = false;
+        log("runFlag: " + task.runFlag);
+        log("main stop");
+    }
+
+    static class MyTask implements Runnable {
+
+        // boolean runFlag = true;
+        volatile boolean runFlag = true;
+
+        @Override
+        public void run() {
+            log("task Start");
+            while (runFlag) {
+                // runFlag가 false로 변하면 탈출
+            }
+            log("task End");
+        }
+
+    }
+
+}
