cc2c7411e80abed8100707f7e8207b7cc1f4d6f0
KROOKIMWOOBIN <woodol4540@naver.com>
2025-12-27 19:41:13 +0900

    20251227

---
+++ src/main/java/javacore/thread/sync/BankAccountV3.java
@@ -0,0 +1,38 @@
+package javacore.thread.sync;
+
+import static javacore.thread.util.MyLogger.log;
+import static javacore.thread.util.ThreadUtils.sleep;
+
+public class BankAccountV3 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV3(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdrew(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+        synchronized (this) {
+            // 검증 로직
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                return false;
+            }
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            // 출금 시작
+            sleep(1_000);
+            balance = balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        }
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public synchronized      int getBalance() {
+        return balance;
+    }
+}
