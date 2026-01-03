7816c48af09e793b07f894340081bc203f624733
KROOKIMWOOBIN <woodol4540@naver.com>
2026-01-03 17:20:26 +0900

    20260103

--- src/main/java/javacore/etc/Main.java
+++ src/main/java/javacore/etc/Main.java
@@ -1,17 +1,5 @@
 package javacore.etc;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
-        List<Integer> intList = new ArrayList<>();
-        Batch batch = new Batch(intList);
-    }
-    private static class Batch {
-        private List<?> list;
-        Batch(List<?> list) {
-            this.list = list;
-        }
-    }
+
 }
