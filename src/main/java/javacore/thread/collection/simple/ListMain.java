56ec5dd40d0eb8a59f5f7dcebf04e37ae9b7c683
KROOKIMWOOBIN <woodol4540@naver.com>
2026-01-10 00:40:21 +0900

    20260110

---
+++ src/main/java/javacore/thread/collection/simple/ListMain.java
@@ -0,0 +1,41 @@
+package javacore.thread.collection.simple;
+
+import javacore.thread.collection.simple.list.BasicList;
+import javacore.thread.collection.simple.list.SimpleList;
+import javacore.thread.collection.simple.list.SyncList;
+
+import static javacore.thread.util.MyLogger.log;
+
+public class ListMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        // test(new BasicList());
+        test(new SyncList());
+    }
+
+    private static void test(SimpleList list) throws InterruptedException {
+        log(list.getClass().getSimpleName());
+        Runnable addA = new Runnable() {
+            @Override
+            public void run() {
+                list.add("A");
+                log("Thread-1: list.add(\"A\")");
+            }
+        };
+        Runnable addB = new Runnable() {
+            @Override
+            public void run() {
+                list.add("B");
+                log("Thread-2: list.add(\"B\")");
+            }
+        };
+        Thread thread1 = new Thread(addA, "Thread-1");
+        Thread thread2 = new Thread(addB, "Thread-2");
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("list = " + list);
+    }
+
+}
