9325eb8ff07f49a976a7fb95a3c5f748eadac8d9
KROOKIMWOOBIN <woodol4540@naver.com>
2026-03-17 21:10:00 +0900

    20260317

--- src/main/java/javacore/lambda/MyStreamMain.java
+++ src/main/java/javacore/lambda/MyStreamMain.java
@@ -1,4 +1,17 @@
 package javacore.lambda;
 
+import lombok.Getter;
+
 public class MyStreamMain {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Getter
+    static class Student {
+        private String name;
+        private int score;
+    }
+
 }
