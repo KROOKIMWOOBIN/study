d80ecc5938f1cf6d5af26518e30b9e90001bfa0d
KROOKIMWOOBIN <woodol4540@naver.com>
2026-03-17 21:12:00 +0900

    20260317

--- src/main/java/javacore/lambda/MyStreamMain.java
+++ src/main/java/javacore/lambda/MyStreamMain.java
@@ -3,10 +3,18 @@ package javacore.lambda;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 public class MyStreamMain {
 
     public static void main(String[] args) {
-
+        List<Student> students = List.of(
+                new Student("A", 70),
+                new Student("B", 40),
+                new Student("C", 90),
+                new Student("D", 30)
+        );
+        
     }
 
     @Getter
