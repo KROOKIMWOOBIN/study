ee118b7b645d7ea2c9bb0c48c93174ced79f6ca6
KROOKIMWOOBIN <woodol4540@naver.com>
2026-03-19 21:42:01 +0900

    20260319

--- src/main/java/javacore/lambda/methodref/MethodRefEx1.java
+++ src/main/java/javacore/lambda/methodref/MethodRefEx1.java
@@ -1,13 +1,16 @@
 package javacore.lambda.methodref;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class MethodRefEx1 {
 
     public static void main(String[] args) {
         // 1. 정적 메서드 참조 클래스::정적메서드
-        Supplier<String> staticMethod = Person::greeting;
-        System.out.println("staticMethod = " + staticMethod.get());
+        Supplier<String> staticMethod1 = Person::greeting;
+        System.out.println("staticMethod1 = " + staticMethod1.get());
+
+        Function<String, String> staticMethod2 = Person::greetingWithName;
 
         // 2. 특정 객체의 인스턴스 참조 객체::인스턴스메서드
         Person person = new Person("Kim");
