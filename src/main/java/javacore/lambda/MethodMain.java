0b7d0f62c4c1dde3e5b2bb5191219085902b79a3
KROOKIMWOOBIN <woodol4540@naver.com>
2026-03-19 20:38:00 +0900

    20260319

--- src/main/java/javacore/lambda/MethodMain.java
+++ src/main/java/javacore/lambda/MethodMain.java
@@ -1,4 +1,16 @@
 package javacore.lambda;
 
+import java.util.function.BinaryOperator;
+
 public class MethodMain {
+
+    public static void main(String[] args) {
+        BinaryOperator<Integer> add = MethodMain::sum;
+        System.out.println(add.apply(5, 10));
+    }
+
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+
 }
