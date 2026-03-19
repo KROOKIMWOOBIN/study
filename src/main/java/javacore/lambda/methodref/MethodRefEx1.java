a2dec40ca53e70f6087f74bca165c37847202e50
KROOKIMWOOBIN <woodol4540@naver.com>
2026-03-19 21:31:00 +0900

    20260319

--- src/main/java/javacore/lambda/methodref/MethodRefEx1.java
+++ src/main/java/javacore/lambda/methodref/MethodRefEx1.java
@@ -1,10 +1,13 @@
 package javacore.lambda.methodref;
 
+import java.util.function.Supplier;
+
 public class MethodRefEx1 {
 
     public static void main(String[] args) {
         // 1. 정적 메서드 참조
-        
+        Supplier<String> staticMethod = Person::greeting;
+        System.out.println("staticMethod = " + staticMethod.get());
     }
 
 }
