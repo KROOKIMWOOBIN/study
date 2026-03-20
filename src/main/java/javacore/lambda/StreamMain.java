7aa6b075ca65cacbcd37026386031d15a29c75c1
KROOKIMWOOBIN <woodol4540@naver.com>
2026-03-20 22:29:00 +0900

    20260320

--- src/main/java/javacore/lambda/StreamMain.java
+++ src/main/java/javacore/lambda/StreamMain.java
@@ -1,12 +1,17 @@
 package javacore.lambda;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamMain {
 
     public static void main(String[] args) {
         List<String> names = List.of("Apple", "Banana", "Berry", "Tomato");
-        names.stream()
+        Stream<String> stream = names.stream();
+        List<String> result = stream.filter(name -> name.startsWith("B"))
+                .map(String::toUpperCase)
+                .toList();
+        
     }
 
 }
